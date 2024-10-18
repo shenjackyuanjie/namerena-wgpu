745095454eb6f6b25737d8c96442f8ebc0fc4bf1
shenjack <3695888@qq.com>
2024-10-19 02:04:47 +0800

    这啥玩意啊.png

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ const BLOCK_SIZE: usize = 256;
 
 const PROGRAM_SOURCE: &str = include_str!("./program.wgsl");
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let tokio_rt = tokio::runtime::Builder::new_multi_thread()
         .worker_threads(4)
         .enable_all()
@@ -22,21 +22,77 @@ fn main() -> Result<()>{
     Ok(())
 }
 
+type ShaderBlock = [u32; BLOCK_SIZE];
+
+trait ShaderBlockExt {
+    fn from_str(s: &str) -> Self;
+    fn as_buffer(&self) -> &[u8];
+}
+
+impl ShaderBlockExt for ShaderBlock {
+    fn from_str(s: &str) -> Self {
+        let mut result = [0; BLOCK_SIZE];
+        for (i, c) in s.chars().enumerate() {
+            result[i] = c as u32;
+        }
+        result
+    }
+    fn as_buffer(&self) -> &[u8] {
+        // u32 -> u8; 4 * n
+        let ptr = self.as_ptr() as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, BLOCK_SIZE * 4) }
+    }
+}
+
+fn str_as_buffer(s: &str) -> &[u8] {
+    // 先转换成 u32 数组
+    let datas = s.chars().map(|c| c as u32).collect::<Vec<u32>>();
+    // 再转换成 u8 数组
+    let ptr = datas.as_ptr() as *const u8;
+    unsafe { std::slice::from_raw_parts(ptr, datas.len() * 4) }
+}
+
+#[repr(C)]
+struct Lens<'a> {
+    team_len: u32,
+    work_count: u32,
+    name_lens: &'a [u32],
+}
+
+impl<'a> Lens<'a> {
+    pub fn as_buffer(&self) -> &[u8] {
+        let ptr = self as *const Self as *const u8;
+        unsafe { std::slice::from_raw_parts(ptr, std::mem::size_of::<Self>()) }
+    }
+    pub fn new(team_len: u32, work_count: u32, name_lens: &'a [u32]) -> Self {
+        Self {
+            team_len,
+            work_count,
+            name_lens,
+        }
+    }
+}
+
 async fn async_main() -> Result<()> {
     // 进行一个中文编程
     let 实例 = wgpu::Instance::default();
 
-    let 适配器 = 实例.request_adapter(&wgpu::RequestAdapterOptions::default()).await.expect(" 无法找到适配器");
+    let 适配器 = 实例
+        .request_adapter(&wgpu::RequestAdapterOptions::default())
+        .await
+        .expect(" 无法找到适配器");
 
-    let (设备, 队列) = 适配器.request_device(
-        &wgpu::DeviceDescriptor {
-            label: Some("GPU"),
-            required_features: wgpu::Features::empty(),
-            required_limits: wgpu::Limits::downlevel_defaults(),
-            memory_hints: wgpu::MemoryHints::Performance,
-        },
-        None,
-    ).await?;
+    let (设备, 队列) = 适配器
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: Some("GPU"),
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::downlevel_defaults(),
+                memory_hints: wgpu::MemoryHints::Performance,
+            },
+            None,
+        )
+        .await?;
 
     let cs_moudle = 设备.create_shader_module(wgpu::ShaderModuleDescriptor {
         label: Some("计算着色器"),
@@ -52,21 +108,92 @@ async fn async_main() -> Result<()> {
         entry_point: "main",
     };
     let 计算管线 = 设备.create_compute_pipeline(&pipline_descriptor);
-    
-    let buffer = 设备.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("输入缓冲区"),
-        contents: &[], // &[u8]``
+
+    let works = { vec!["x"; 1] };
+    let raw_works = works.iter().map(|s| s.as_bytes()).collect::<Vec<&[u8]>>();
+    let filled_works = {
+        let mut vecs = vec![];
+        for work in raw_works.iter() {
+            let mut vec = vec![0; BLOCK_SIZE];
+            vec[..work.len()].copy_from_slice(work);
+            vecs.extend_from_slice(&vec);
+        }
+        vecs
+    };
+    let work_count = works.len() as u32;
+    let raw_lens = raw_works
+        .iter()
+        .map(|s| s.len() as u32)
+        .collect::<Vec<u32>>();
+    let lens = Lens::new(1, work_count, &raw_lens);
+
+    let raw_data = str_as_buffer("x");
+
+    let team_name_buffer = 设备.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("队名"),
+        contents: raw_data, // &[u8]``
+        usage: BufferUsages::STORAGE,
+    });
+    let names_buffer = 设备.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("队员"),
+        contents: &filled_works, // &[u8]``
+        usage: BufferUsages::STORAGE,
+    });
+    let lens_buffer = 设备.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("长度"),
+        contents: lens.as_buffer(), // &[u8]``
+        usage: BufferUsages::STORAGE,
+    });
+    let result_buffer = 设备.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("结果"),
+        size: BLOCK_SIZE as u64,
         usage: BufferUsages::STORAGE | BufferUsages::COPY_SRC,
+        mapped_at_creation: false,
     });
     let bind_group_layout_0 = 计算管线.get_bind_group_layout(0);
     let bind_group_0 = 设备.create_bind_group(&wgpu::BindGroupDescriptor {
         label: Some("绑定组"),
         layout: &bind_group_layout_0,
-        entries: &[wgpu::BindGroupEntry {
-            binding: 0,
-            resource: buffer.as_entire_binding(),
-        }],
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: team_name_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: names_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: lens_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 3,
+                resource: result_buffer.as_entire_binding(),
+            },
+        ],
     });
+
+    let limits = 设备.limits();
+    println!("x轴最大工作组数: {}", limits.max_compute_workgroup_size_x);
+    println!("y轴最大工作组数: {}", limits.max_compute_workgroup_size_y);
+    println!("z轴最大工作组数: {}", limits.max_compute_workgroup_size_z);
+    println!(
+        "最大工作组数: {}",
+        limits.max_compute_workgroups_per_dimension
+    );
+
+    let mut encoder = 设备.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+    {
+        let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: None,
+            timestamp_writes: None,
+        });
+        cpass.set_pipeline(&计算管线);
+        cpass.set_bind_group(0, &bind_group_0, &[]);
+        cpass.insert_debug_marker("计算");
+        cpass.dispatch_workgroups(1, 1, 1);
+    }
 
     Ok(())
 }
--- src/program.wgsl
+++ src/program.wgsl
@@ -1,25 +1,28 @@
-
+// 块大小
 const block_size = 256;
+alias Block = array<u32, 256>;
 
-struct String {
-    data: array<u32, block_size>,
+struct Lens {
+    team_len: u32,
+    work_count: u32,
+    name_lens: array<u32>
 }
 
-struct WorkTeam {
-    // 奢侈点, 直接 u32 当 u8 用
-    team: array<u32, block_size>,
-    // 一堆名字
-    names: array<String>,
-}
+/// 输入
+// 统一的队名
+@group(0) @binding(0) var<storage, read> team_name: array<u32>;
+// 每个任务分别的队员名
+@group(0) @binding(1) var<storage, read> names: array<Block>;
+// 各种长度
+@group(0) @binding(2) var<storage, read> lens: Lens;
 
-// 输入
-@group(0) @binding(0) var<storage, read_write> team: WorkTeam;
-
-// 输出
-@group(0) @binding(1) var result: array<array<u32, block_size>>;
+/// 输出
+@group(0) @binding(3) var<storage, read_write> result: array<Block>;
 
 // 主函数
 @compute @workgroup_size(16)
 fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
-
+    let a = team_name[global_id.x % lens.team_len];
+    let b = names[global_id.x / lens.team_len][global_id.x % lens.team_len];
+    result[global_id.x][0] = 1u;
 }
